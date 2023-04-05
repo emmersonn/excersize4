387adf4e31fedb2c64e634f081f294402c20a872
sfishkin13 <95653679+sfishkin13@users.noreply.github.com>
2023-04-05 15:01:45 -0400

    Update exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,5 +1,4 @@
 def hello():
-<<<<<<< HEAD
     print('hello')
     
 def goodbye():
@@ -7,6 +6,3 @@ def goodbye():
     
 hello()
 goodbye()
-=======
-    print('hello')
->>>>>>> 60cdabe1a66b589e011f9a600dbac4b6d38b556b
